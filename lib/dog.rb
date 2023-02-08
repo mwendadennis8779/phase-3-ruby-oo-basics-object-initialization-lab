2da3641f2b71b6ba4c71e9beb7c87b92abb60dfd
Dennis <mwendadennis8779@gmail.com>
2023-02-08 16:29:21 +0300

    Completed assignment

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+    attr_accessor :breed
+    attr_accessor :name
+
+    def initialize(name,breed = "Mutt")
+        @name = name
+        @breed = breed
+    end
+
+end
+
+fido = Dog.new("Fido", "Pug")
+
+
